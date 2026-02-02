e4aedc673f7d4adb241ffbb9a90fe7981c742379
Alan Thomas <alanthomas171@gmail.com>
2026-02-02 00:42:38 +0000

    Added Browser Validation through form

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const taskList = document.querySelector(".task-list");
 const inputTask = document.querySelector(".input-task");
-const addButton = document.querySelector(".add-button");
+const todoForm = document.querySelector(".todo-form");
 let newTask = "";
 const tasks = [];
-addButton.addEventListener("click", onClickAddButton);
-function onClickAddButton() {
+todoForm.addEventListener("submit", onClickAddButton);
+function onClickAddButton(event) {
+  event.preventDefault();
   newTask = inputTask.value;
 
   const recentTask = document.createElement("li");
