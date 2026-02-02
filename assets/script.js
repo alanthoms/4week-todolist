382400287fd9288359e6bef785d3148d385a3c9e
Alan Thomas <alanthomas171@gmail.com>
2026-02-02 02:54:56 +0000

    Add edit button

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,60 @@
 const taskList = document.querySelector(".task-list");
 const inputTask = document.querySelector(".input-task");
 const todoForm = document.querySelector(".todo-form");
-let newTask = "";
+
 let tasks = [];
 
 todoForm.addEventListener("submit", onClickAddButton);
 
 function onClickAddButton(event) {
   event.preventDefault();
-  newTask = inputTask.value;
+  let newTask = inputTask.value;
   if (tasks.includes(newTask)) {
     alert("Enter unique task");
     return;
   }
   tasks.push(newTask);
   const recentTask = document.createElement("li");
+
+  const recentTaskSpan = document.createElement("span");
   const editButton = document.createElement("button");
   editButton.innerHTML = "edit";
   const deleteButton = document.createElement("button");
   deleteButton.innerHTML = "delete";
-  recentTask.innerHTML = newTask;
+  recentTaskSpan.innerHTML = newTask;
+
+  let editInput = document.createElement("input");
+
+  let saveButton = document.createElement("button");
+
+  editInput.value = recentTaskSpan.innerHTML;
+  saveButton.innerHTML = "Save";
 
   deleteButton.addEventListener("click", () => {
     recentTask.remove();
     tasks = tasks.filter((task) => task !== newTask);
   });
 
-  recentTask.append(editButton, deleteButton);
+  editButton.addEventListener("click", () => {
+    recentTaskSpan.style.display = "none";
+    editButton.style.display = "none";
+    editInput.value = newTask;
+    recentTask.append(editInput, saveButton);
+  });
+
+  saveButton.addEventListener("click", () => {
+    if (tasks.indexOf(recentTaskSpan.innerHTML) !== -1) {
+      tasks[tasks.indexOf(recentTaskSpan.innerHTML)] = editInput.value;
+    }
+
+    recentTaskSpan.innerHTML = editInput.value;
+    newTask = editInput.value;
+    recentTaskSpan.style.display = "inline";
+    editButton.style.display = "inline";
+
+    editInput.remove();
+    saveButton.remove();
+  });
+  recentTask.append(recentTaskSpan, editButton, deleteButton);
   taskList.appendChild(recentTask);
 }
