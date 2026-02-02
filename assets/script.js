b3a14e49652e090361b093fa493e4bdc01d340e0
Alan Thomas <alanthomas171@gmail.com>
2026-02-02 00:53:31 +0000

    Added unique verification to input and alert for that

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,19 @@ const inputTask = document.querySelector(".input-task");
 const todoForm = document.querySelector(".todo-form");
 let newTask = "";
 const tasks = [];
+
 todoForm.addEventListener("submit", onClickAddButton);
 function onClickAddButton(event) {
   event.preventDefault();
   newTask = inputTask.value;
+  if (tasks.includes(newTask)) {
+    alert("Enter unique task");
+  } else {
+    tasks.push(newTask);
+    const recentTask = document.createElement("li");
 
-  const recentTask = document.createElement("li");
+    recentTask.innerHTML = newTask;
 
-  recentTask.innerHTML = newTask;
-
-  taskList.appendChild(recentTask);
+    taskList.appendChild(recentTask);
+  }
 }
