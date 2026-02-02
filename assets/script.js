4e2311047c3462d9a8e8adeba192d6b8e98f1fa2
Alan Thomas <alanthomas171@gmail.com>
2026-02-02 01:22:58 +0000

    Add delete button

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,30 @@ const taskList = document.querySelector(".task-list");
 const inputTask = document.querySelector(".input-task");
 const todoForm = document.querySelector(".todo-form");
 let newTask = "";
-const tasks = [];
+let tasks = [];
 
 todoForm.addEventListener("submit", onClickAddButton);
+
 function onClickAddButton(event) {
   event.preventDefault();
   newTask = inputTask.value;
   if (tasks.includes(newTask)) {
     alert("Enter unique task");
-  } else {
-    tasks.push(newTask);
-    const recentTask = document.createElement("li");
-
-    recentTask.innerHTML = newTask;
-
-    taskList.appendChild(recentTask);
+    return;
   }
+  tasks.push(newTask);
+  const recentTask = document.createElement("li");
+  const editButton = document.createElement("button");
+  editButton.innerHTML = "edit";
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "delete";
+  recentTask.innerHTML = newTask;
+
+  deleteButton.addEventListener("click", () => {
+    recentTask.remove();
+    tasks = tasks.filter((task) => task !== newTask);
+  });
+
+  recentTask.append(editButton, deleteButton);
+  taskList.appendChild(recentTask);
 }
